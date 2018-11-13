610e9fda1b67bd82514d12e11a603b329dc480e1
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2018-11-13 12:33:25 +0700

    gagal mailgun

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'ady.fatkhurozi@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'admin@larapus.com'),
         'name' => env('MAIL_FROM_NAME', 'Admin Larapus'),
     ],
 
